5e5f7e710d8e5033bc7bad723c21145b686a817a
Muhammed AbdulHadi <Tp085418@mail.apu.edu.my>
2026-05-24 16:29:48 +0800

    Update RefuelTruck.java: correct mutual exclusion, correct refuel timings

--- RefuelTruck.java
+++ RefuelTruck.java
@@ -1,10 +1,11 @@
 /**
  * RefuelTruck allows only one plane to refuel at a time.
+ * Uses synchronized for mutual exclusion.
  */
 public class RefuelTruck {
     public synchronized void refuel(String planeName) throws InterruptedException {
         Logger.log(planeName + "-Refuel", "Refueling aircraft.");
-        Thread.sleep(Utils.randomBetween(1000, 2000));
+        Thread.sleep(Utils.randomBetween(700, 1000)); // Updated timing
         Logger.log(planeName + "-Refuel", "Refueling complete.");
     }
 }
