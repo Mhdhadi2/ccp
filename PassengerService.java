f62671bf3da0b7119a84c93a1656271097cc4165
Muhammed AbdulHadi <Tp085418@mail.apu.edu.my>
2026-05-24 16:22:15 +0800

    Update PassengerService.java: separate disembark/board logic, assignment-correct timings

--- PassengerService.java
+++ PassengerService.java
@@ -1,23 +1,26 @@
 /**
  * PassengerService simulates passenger disembarkation and boarding.
+ * Each is timed independently to more closely simulate real world.
  */
 public class PassengerService extends Thread {
     private final String planeName;
-    private final int passengerCount;
+    private final int passengersToDisembark;
+    private final int passengersToBoard;
 
-    public PassengerService(String planeName, int passengerCount) {
+    public PassengerService(String planeName, int passengersToDisembark, int passengersToBoard) {
         super(planeName + "-Passengers");
         this.planeName = planeName;
-        this.passengerCount = passengerCount;
+        this.passengersToDisembark = passengersToDisembark;
+        this.passengersToBoard = passengersToBoard;
     }
 
     @Override
     public void run() {
         try {
-            Logger.log(getName(), passengerCount + " passengers disembarking.");
-            Thread.sleep(Utils.randomBetween(1000, 2000));
-            Logger.log(getName(), "Passengers boarding.");
-            Thread.sleep(Utils.randomBetween(1000, 2000));
+            Logger.log(getName(), passengersToDisembark + " passengers disembarking.");
+            Thread.sleep(Utils.randomBetween(600, 1000));
+            Logger.log(getName(), passengersToBoard + " passengers boarding.");
+            Thread.sleep(Utils.randomBetween(600, 1000));
             Logger.log(getName(), "Passenger service complete.");
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
